47f4f9f1eb3674b9ac9d193db7d5744cd3229b1f
ErnestStec <144111990+ErnestStec@users.noreply.github.com>
2023-10-04 08:58:45 +0200

    Add files via upload

---
+++ Opdracht JavaScript/script.js
@@ -0,0 +1,16 @@
+function veranderKleur() {
+    var h1Element = document.getElementById('mijnH1');
+    h1Element.style.color = 'blue';
+}
+
+function veranderTekst() {
+        var h1Element = document.getElementById('mijnH1');
+        h1Element.innerText = 'Nieuwe tekst JavaScript';
+}
+
+function reset() {
+        var h1Element = document.getElementById('mijnH1');
+        h1Element.style.color = 'black';
+        var h1Element = document.getElementById('mijnH1');
+        h1Element.innerText = 'Opdracht 5.1 - JavaScript';
+}
