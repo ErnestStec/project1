de266c3641406a0d48167c90e7a3d7d1f81e7a28
ErnestStec <144111990+ErnestStec@users.noreply.github.com>
2023-10-12 10:14:39 +0200

    Add files via upload

---
+++ 6.4 js/app.js
@@ -0,0 +1,14 @@
+function calculate(num1, num2) {
+    const sum = num1 + num2;
+    console.log("Oefening 1 - Resultaat: " + sum);
+}
+
+function multiplyByFive(num) {
+    const result = num * 5;
+    console.log("Oefening 2 - Resultaat: " + result);
+}
+
+function calculateMinutes(seconds) {
+    const minutes = seconds / 60;
+    console.log("Oefening 3 - Resultaat: " + minutes + " minuten");
+}
