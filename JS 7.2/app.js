d8f2e013de4d5576b3476616aefbad5fdaa8f265
ErnestStec <144111990+ErnestStec@users.noreply.github.com>
2023-10-26 09:57:09 +0200

    Add files via upload

---
+++ JS 7.2/app.js
@@ -0,0 +1,13 @@
+let product = 100;
+
+let budget = prompt("Hoeveel geld heb je?", 0);
+
+let ResultP = document.getElementById("ResultParagraph");
+if  (product >= budget) {
+    ResultP.innerHTML = "Helaas, te weining geld";
+    ResultP.style.color = "red"
+}
+else {
+    ResultP.innerHTML = "U heeft genoeg geld";
+    ResultP.style.color = "green";
+}
