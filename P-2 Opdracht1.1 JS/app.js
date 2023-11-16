7b6bb1fbf15432f04d496c60f49ccdbcc40490bb
ErnestStec <144111990+ErnestStec@users.noreply.github.com>
2023-11-16 10:31:35 +0100

    Add files via upload

---
+++ P-2 Opdracht1.1 JS/app.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById("cookieWall");
+
+function showModal() {
+    modal.style.display = "block";
+}
+
+function removeModal() {
+    modal.style.display = "none";
+}
+
+showModal();
